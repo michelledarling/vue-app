fa606a6e6ee2a1e21ddc967f947572ac2257cf5d
michelledarling <michelle.darling@hook42.com>
2020-08-18 15:44:25 -0500

    init

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="app container">
+    <ul>
+      <li>{{ number + 1 }}</li>
+      <li>Value of ok var is: {{ ok ? 'true' : 'false' }}</li>
+      <li>{{ message.split('').reverse().join('') }}</li>
+    </ul>
+    <span v-if="visible">Now you see me</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "App",
+    data: () => ({
+      message: "Powered by MD!",
+      number: 5,
+      ok: true,
+      visible: true,
+    })
+  };
+</script>
+
+<style>
+  .app {
+    margin: 0 auto;
+    font-family: sans-serif;
+    background-color: #ccf7e2;
+    padding: 10px;
+    border-radius: 5px;
+    max-width: 500px;
+  }
+</style>
